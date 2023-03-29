62a36da39df31803a277c822ca33e3eb8e62935d
JulieV <Trenzelek2009@yandex.ru>
2023-03-29 19:25:27 +0300

    rel half db

---
+++ db/models/candidate.js
@@ -0,0 +1,53 @@
+const { Model } = require('sequelize');
+
+module.exports = (sequelize, DataTypes) => {
+  class Candidate extends Model {
+    static associate({
+      Interview, History, Screencall, Videocall, Comment,
+    }) {
+      this.hasMany(Interview, { foreignKey: 'candidate_id' });
+      this.hasMany(History, { foreignKey: 'candidate_id' });
+      this.hasMany(Screencall, { foreignKey: 'candidate_id' });
+      this.hasMany(Videocall, { foreignKey: 'candidate_id' });
+      this.hasMany(Comment, { foreignKey: 'candidate_id' });
+    }
+  }
+  Candidate.init(
+    {
+      image: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      experience: {
+        allowNull: false,
+        type: DataTypes.INTEGER,
+      },
+      number: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      email: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+        unique: true,
+      },
+      reject_status: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+      },
+      handed: {
+        allowNull: false,
+        type: DataTypes.BOOLEAN,
+      },
+    },
+    {
+      sequelize,
+      modelName: 'Candidate',
+    },
+  );
+  return Candidate;
+};
---
+++ db/models/interviewer.js
@@ -0,0 +1,39 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Interviewer extends Model {
+    static associate({ Interview }) {
+      this.hasMany(Interview, { foreignKey: "interviewer_id" });
+    }
+  }
+  Interviewer.init(
+    {
+      email: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+        unique: true,
+      },
+      name: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      number: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      zoom: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+      skype: {
+        allowNull: false,
+        type: DataTypes.TEXT,
+      },
+    },
+    {
+      sequelize,
+      modelName: "Interviewer",
+    }
+  );
+  return Interviewer;
+};
