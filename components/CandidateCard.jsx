c94c0596a687822473605fa1c5805491201d5321
Ivan <ivanpakhtusov@gmail.com>
2023-03-30 21:07:54 +0500

    created 'CandidateCard'

--- components/CandidateCard.jsx
+++ components/CandidateCard.jsx
@@ -1,17 +1,20 @@
 const React = require('react');
+const Layout = require('./Layout');
 
 function CandidateCard({ candidate }) {
   return (
-    <div className="card">
-      <img src={candidate.img} className="card-img-bottom" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">{candidate.name}</h5>
-        <p className="card-text">{candidate.experience}</p>
-        <p className="card-text">{candidate.email}</p>
-        <p className="card-text">{candidate.number}</p>
-        <button type="submit">Оставить комментарий</button>
+    <Layout>
+      <div className="card">
+        <img src={candidate.image} className="card-img-bottom" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">{candidate.name}</h5>
+          <p className="card-text">Years' experience: {candidate.experience}</p>
+          <p className="card-text">{candidate.email}</p>
+          <p className="card-text">{candidate.number}</p>
+          <button type="submit">Оставить комментарий</button>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
 
