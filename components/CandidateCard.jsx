d92ff03fb199bd73c6393aa9d75a3f7322e505a0
Klim <xarkoni@gmail.com>
2023-03-30 23:29:20 +0300

    History render on Card

--- components/CandidateCard.jsx
+++ components/CandidateCard.jsx
@@ -1,16 +1,34 @@
 const React = require('react');
 const Layout = require('./Layout');
 
-function CandidateCard({ candidate }) {
+function CandidateCard({ candidate, history = {} }) {
   return (
     <Layout>
-      <div className="card">
+      <div className="card" id={candidate.id}>
         <img src={candidate.image} className="card-img-bottom" alt="..." />
         <div className="card-body">
-          <h5 className="card-title">{candidate.name}</h5>
+          <h5 className="card-title">Name: {candidate.name}</h5>
           <p className="card-text">Years' experience: {candidate.experience}</p>
-          <p className="card-text">{candidate.email}</p>
-          <p className="card-text">{candidate.number}</p>
+          <p className="card-text">Email: {candidate.email}</p>
+          <p className="card-text">Number: {candidate.number}</p>
+          <h1>History:</h1>
+          <p className="card-text">
+            Invite status: {history.invite && `✅ ${history.invite}`}
+          </p>
+          <p className="card-text">
+            Screencall status:{' '}
+            {history.screencall && `✅ ${history.screencall}`}
+          </p>
+          <p className="card-text">
+            Videocall status: {history.videocall && `✅ ${history.videocall}`}
+          </p>
+          <p className="card-text">
+            Offer status: {history.offer_date && `✅ ${history.offer_date}`}
+          </p>
+          <p className="card-text">
+            Offer assign date:{' '}
+            {history.offer_accepted && `✅ ${history.offer_accepted}`}
+          </p>
           <button type="submit">Оставить комментарий</button>
         </div>
       </div>
--- routes/infoCard.route.js
+++ routes/infoCard.route.js
@@ -3,13 +3,21 @@ const React = require('react');
 const RDM = require('react-dom/server');
 
 const CandidateCard = require('../components/CandidateCard');
-const { Candidate } = require('../db/models');
+const { Candidate, History } = require('../db/models');
 
 router.get('/:id', async (req, res) => {
   try {
     const { id } = req.params;
     const candidate = await Candidate.findOne({ where: { id }, raw: true });
-    res.renderComponent(CandidateCard, { title: 'Candidate page', candidate });
+    const { dataValues: history } = await History.findOne({
+      where: { candidate_id: id },
+    });
+    console.log(history);
+    res.renderComponent(CandidateCard, {
+      title: 'Candidate page',
+      candidate,
+      history,
+    });
   } catch ({ message }) {
     res.json(message);
   }
