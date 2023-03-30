b5f8cbf506d0c00825402d224faa7447acf70685
Klim <xarkoni@gmail.com>
2023-03-30 17:19:40 +0300

    cards

--- components/CandidatesItem.jsx
+++ components/CandidatesItem.jsx
@@ -1,12 +1,13 @@
 const React = require('react');
 
-function CandidateItem() {
+function CandidateItem({ candidate }) {
   return (
-    <div className="card" style="width: 18rem;">
-      <img src={candidate.img} className="card-img-top" alt="..." />
+    <div className="card" style={{ width: '18rem' }}>
+      <img src={candidate.image} className="card-img-top" alt="..." />
       <div className="card-body">
         <h5 className="card-title">{candidate.name}</h5>
         <p className="card-text">{candidate.email}</p>
+        <p className="card-text">{candidate.experience}</p>
         <a href="#" className="btn btn-primary">
           Подробнее
         </a>
